89c2db4bc3b1dd167fb7402aa83f699bf6b6098b
lusine <movsisianlusine@gmail.com>
2026-04-20 15:21:49 +0400

    Stoped tracking Qt Creator local settings

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_MyQtProject_FILE C:/Users/user/source/repos/2048/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/MyQtProject.exe)
-set(__QT_DEPLOY_TARGET_MyQtProject_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_MyQtProject_RUNTIME_DLLS C:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
